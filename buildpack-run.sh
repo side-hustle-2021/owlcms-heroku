79905a825dbdc25297ae050738ffa0153e968385
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-01-10 16:19:02 -0700

    changed the jat file storage location to dropbox

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://srv-store4.gofile.io/download/IHMit2/owlcms-5.0.0-rc01.jar" -o owlcms.jar
+curl -L "https://www.dropbox.com/s/uf5fdjz8gfzxnhq/owlcms-5.0.0-rc02.jar?dl=1" -o owlcms.jar
