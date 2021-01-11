b00a9c9c364b77ddea26f169fe9ec35f3933a58d
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-01-10 19:50:10 -0700

    5.0.0-rc03 release changes

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://www.dropbox.com/s/uf5fdjz8gfzxnhq/owlcms-5.0.0-rc02.jar?dl=1" -o owlcms.jar
+curl -L "https://www.dropbox.com/s/2yoxqc797ukte16/owlcms-5.0.0-rc03.jar?dl=1" -o owlcms.jar
