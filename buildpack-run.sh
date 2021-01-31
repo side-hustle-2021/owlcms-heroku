5662dd63bf91e1f8c917d4e48c8f33996862cb3b
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-01-31 14:57:01 -0700

    5.1.2-rc01 bug fix - updated active query

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://www.dropbox.com/s/u6d56pvq6avg1r9/owlcms-5.1.1-rc02.jar?dl=1" -o owlcms.jar
+curl -L "https://www.dropbox.com/s/y7z3i4kkmw2ph6q/owlcms-5.1.2-rc01.jar?dl=1" -o owlcms.jar
