09bfcd7339012e4832ff2c99621198ee9945e2bf
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-02-07 02:05:13 -0700

    5.2.0-rc01 customregister api added

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://www.dropbox.com/s/y7z3i4kkmw2ph6q/owlcms-5.1.2-rc01.jar?dl=1" -o owlcms.jar
+curl -L "https://www.dropbox.com/s/bjt7312565rmxhh/owlcms-5.2.0-rc01.jar?dl=1" -o owlcms.jar
