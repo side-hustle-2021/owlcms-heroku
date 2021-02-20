44e2fbd5be3ac2c677c010a2d1a456e84afe4f9a
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-02-20 16:12:12 -0700

    5.4.0-rc01: API registration using password and cors API login

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://www.dropbox.com/s/g4v2mojrangrjd9/owlcms-5.3.0-rc01.jar?dl=1" -o owlcms.jar
+curl -L "https://www.dropbox.com/s/qq3emn13hu9l7xz/owlcms-5.4.0-rc01.jar?dl=1" -o owlcms.jar
