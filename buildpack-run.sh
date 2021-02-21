048f61568d159ffb42716c76ad1d211a9221788a
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-02-20 18:38:12 -0700

    5.4.1-rc01: customlogout api added

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://www.dropbox.com/s/qq3emn13hu9l7xz/owlcms-5.4.0-rc01.jar?dl=1" -o owlcms.jar
+curl -L "https://www.dropbox.com/s/a24v87nlt4bpzwg/owlcms-5.4.1-rc01.jar?dl=1" -o owlcms.jar
