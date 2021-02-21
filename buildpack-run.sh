92f278cff4b800e084aa4344e32ffea4cbf9c7c9
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-02-20 19:21:12 -0700

    5.4.3 rebuilding jar

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://www.dropbox.com/s/w4i41ant1dyw5n2/owlcms-5.4.2.jar?dl=1" -o owlcms.jar
+curl -L "https://www.dropbox.com/s/umea4nv9hensjb5/owlcms-5.4.3.jar?dl=1" -o owlcms.jar
