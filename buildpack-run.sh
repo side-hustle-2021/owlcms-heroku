6d75f529207aee61f6f5448b922bc3d95bea4195
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-02-27 18:52:08 -0700

    5.5.0 username athlete mapping

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://www.dropbox.com/s/umea4nv9hensjb5/owlcms-5.4.3.jar?dl=1" -o owlcms.jar
+curl -L "https://www.dropbox.com/s/ehszjxdld8el9rz/owlcms-5.5.0.jar?dl=1" -o owlcms.jar
