f5486149c43ca9a99d9de42a011aea80c05b87ee
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-03-09 00:07:35 -0700

    5.6.0 - Athlete Card changes

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://www.dropbox.com/s/ehszjxdld8el9rz/owlcms-5.5.0.jar?dl=1" -o owlcms.jar
+curl -L "https://www.dropbox.com/s/om9503egb37vc8f/owlcms-5.6.0.jar?dl=1" -o owlcms.jar
